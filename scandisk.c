1f0168e34b85a3d030268f7d46705e7a4aea1fdb
cosc301 <lpentecost>
2015-12-10 14:50:48 -0500

    Initial attempt at fixing problem 1

--- scandisk.c
+++ scandisk.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "bootsect.h"
 #include "bpb.h"
@@ -20,6 +21,179 @@ void usage(char *progname) {
     exit(1);
 }
 
+void print_indent(int indent)
+{
+    int i;
+    for (i = 0; i < indent*4; i++)
+	printf(" ");
+}
+
+bool check_size(struct direntry *dirent, struct bpb33 *bpb, uint8_t *image_buf){
+    int meta_size = getulong(dirent->deFileSize);
+    bool has_size_problem = false;
+    uint16_t cluster_size = bpb->bpbBytesPerSec * bpb->bpbSecPerClust;
+    uint32_t fat_size = cluster_size; //
+    uint16_t cluster = getushort(dirent->deStartCluster);
+
+    while(is_valid_cluster(cluster, bpb)){
+        fat_size +=cluster_size;
+        if(fat_size>meta_size){ //this cluster is beyond meta_size, free it
+            set_fat_entry(cluster, FAT12_MASK & CLUST_FREE, image_buf, bpb);
+            has_size_problem = true;
+        }
+        cluster = get_fat_entry(cluster, image_buf, bpb);
+    }
+
+    //check conditions for meta_size in approrpiate range for cluster_size*num_clust
+    if (meta_size > fat_size){
+        putulong(dirent->deFileSize, fat_size);
+        has_size_problem = true;
+        return has_size_problem;
+    } else { //no modifications necessary
+        return has_size_problem; 
+    }
+}
+
+uint16_t access_dirent(struct direntry *dirent, int indent, struct bpb33 *bpb, uint8_t *image_buf)
+{
+    uint16_t followclust = 0;
+
+    int i;
+    char name[9];
+    char extension[4];
+    uint32_t size;
+    uint16_t file_cluster;
+    name[8] = ' ';
+    extension[3] = ' ';
+    memcpy(name, &(dirent->deName[0]), 8);
+    memcpy(extension, dirent->deExtension, 3);
+    if (name[0] == SLOT_EMPTY)
+    {
+	return followclust;
+    }
+
+    /* skip over deleted entries */
+    if (((uint8_t)name[0]) == SLOT_DELETED)
+    {
+	return followclust;
+    }
+
+    if (((uint8_t)name[0]) == 0x2E)
+    {
+	// dot entry ("." or "..")
+	// skip it
+        return followclust;
+    }
+
+    /* names are space padded - remove the spaces */
+    for (i = 8; i > 0; i--) 
+    {
+	if (name[i] == ' ') 
+	    name[i] = '\0';
+	else 
+	    break;
+    }
+
+    /* remove the spaces from extensions */
+    for (i = 3; i > 0; i--) 
+    {
+	if (extension[i] == ' ') 
+	    extension[i] = '\0';
+	else 
+	    break;
+    }
+
+    if ((dirent->deAttributes & ATTR_WIN95LFN) == ATTR_WIN95LFN)
+    {
+	// ignore any long file name extension entries
+	//
+	// printf("Win95 long-filename entry seq 0x%0x\n", dirent->deName[0]);
+    }
+    else if ((dirent->deAttributes & ATTR_VOLUME) != 0) 
+    {
+	printf("Volume: %s\n", name);
+    } 
+    else if ((dirent->deAttributes & ATTR_DIRECTORY) != 0) 
+    {
+        // don't deal with hidden directories; MacOS makes these
+        // for trash directories and such; just ignore them.
+	if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN)
+        {
+	    print_indent(indent);
+    	    printf("%s/ (directory)\n", name);
+            file_cluster = getushort(dirent->deStartCluster);
+            followclust = file_cluster;
+        }
+    }
+    else 
+    {
+        /*
+         * a "regular" file entry
+         * print attributes, size, starting cluster, etc.
+         */
+	int ro = (dirent->deAttributes & ATTR_READONLY) == ATTR_READONLY;
+	int hidden = (dirent->deAttributes & ATTR_HIDDEN) == ATTR_HIDDEN;
+	int sys = (dirent->deAttributes & ATTR_SYSTEM) == ATTR_SYSTEM;
+	int arch = (dirent->deAttributes & ATTR_ARCHIVE) == ATTR_ARCHIVE;
+
+	size = getulong(dirent->deFileSize);
+	print_indent(indent);
+
+        //check for size inconsistencies
+        bool had_size_problem  = check_size(dirent, bpb, image_buf);
+
+        if(had_size_problem){
+	    printf("%s.%s (%u bytes) (starting cluster %d) %c%c%c%c\n", 
+	       name, extension, size, getushort(dirent->deStartCluster),
+	       ro?'r':' ', 
+               hidden?'h':' ', 
+               sys?'s':' ', 
+               arch?'a':' ');
+        }
+    }
+
+    return followclust;
+}
+
+void follow_dir(uint16_t cluster, int indent,
+		uint8_t *image_buf, struct bpb33* bpb)
+{
+    while (is_valid_cluster(cluster, bpb))
+    {
+        struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+        int numDirEntries = (bpb->bpbBytesPerSec * bpb->bpbSecPerClust) / sizeof(struct direntry);
+        int i = 0;
+	for ( ; i < numDirEntries; i++)
+	{
+            
+            uint16_t followclust = access_dirent(dirent, indent, bpb, image_buf);
+            if (followclust)
+                follow_dir(followclust, indent+1, image_buf, bpb);
+            dirent++;
+	}
+
+	cluster = get_fat_entry(cluster, image_buf, bpb);
+    }
+}
+
+
+void traverse_root(uint8_t *image_buf, struct bpb33* bpb)
+{
+    uint16_t cluster = 0;
+
+    struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
+
+    int i = 0;
+    for ( ; i < bpb->bpbRootDirEnts; i++)
+    {
+        uint16_t followclust = access_dirent(dirent, 0, bpb, image_buf);
+        if (is_valid_cluster(followclust, bpb))
+            follow_dir(followclust, 1, image_buf, bpb);
+
+        dirent++;
+    }
+}
 
 int main(int argc, char** argv) {
     uint8_t *image_buf;
@@ -34,10 +208,16 @@ int main(int argc, char** argv) {
 
     // your code should start here...
 
+    //check filesize in metadata vs. FAT clusters
 
+    //print out a list of files whose length is inconsistent
+    //free clusters beyond EOF as defined in metadata
+    //adjust size in metadata if EOF or BAD before end of data according to metadata
+    
+    //deFileSize in direntry
+    //get_fat_entry until EOF or BAD to find cluster size to compare
 
-
-
+    traverse_root(image_buf, bpb); //this call will execute checks inside print_dirent
 
     unmmap_file(image_buf, &fd);
     return 0;
